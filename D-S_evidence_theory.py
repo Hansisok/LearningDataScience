7e8b60af22c4c9c35b5f7e1d90c145a5094372e2
Hans <1156711794@qq.com>
2025-03-23 21:17:15 +0800

    DS继续修改

--- D-S_evidence_theory.py
+++ D-S_evidence_theory.py
@@ -9,35 +9,82 @@ import numpy as np
 '''
 class DStheory:
     # Constructor
-    def __init__(self, Phi: list):
-        self.Phi = Phi
-        self.A_list = []
-        self.m = list(np.ones( 2**len(Phi) ) / (2**len(Phi)) )
+    def __init__(self, Events: list):
+        self.Events = Events
+        self.lenFrame = 2**len(Events)
+        self.BPAs = dict()
 
-    # Basic probability assignment
-    def bpa(self, A: list, m: int):
-        self.m[self.get_Aindex(A)] = m  
-        pass
+    # add a basic probability assignment named 'name' to the frame
+    def addBPA(self, A: list, m: list, name: str):
+        if len(A) != len(m):
+            raise ValueError("The length of A and m must be the same.")
+        if sum(m) != 1:
+            raise ValueError("The sum of m must be 1.")
+        if name in self.BPAs:
+            raise ValueError("The name already exists.")
+        
+        mList = list(np.zeros( self.lenFrame ) / (self.lenFrame) )
+
+        for i in range(len(A)):
+            try:
+                print(self.__getIndex_A(A[i]), A[i], m[i])
+                mList[self.__getIndex_A(A[i])] = m[i]  
+            except:
+                raise ValueError("The event in A must be in the frame.")
+        
+        self.BPAs[name] = mList
+
+    # remove a basic probability assignment named 'name' from the frame
+    def removeBPA(self, name: str):
+        self.mList.pop(name)
 
     # Belief
-    def belief(self, Phi: list, X: list):
+    def belief(self, nameLists: list, name: str):
         pass
 
-    def get_Aindex(self, A: list):
+    # Plausible
+    def plausible(self, nameLists: list, name: str):
+        pass
+
+    # Combine
+    def combine(self, names: list, name: str):
+        combineBPA = list(np.zeros( self.lenFrame ) / (self.lenFrame) )
+        for name in names:
+            pass
+        pass
+
+    def __findIntersection (self, index: int):
+        # 找到交集有且仅有index对应的Event的所有BPA下标列表
+        i0 = 2 ** index
+        level1 = []
+        for i in range(self.lenFrame):
+            level2 = []
+            if i & i0 == i0 and :
+                pass
+        pass
+
+    def __findOR(self, A: list, B: list):
+        pass
+
+    # Show the frame
+    def showFrame(self):
+        print("Frame: ", self.Events)
+        print("BPAs: ", self.BPAs)
+
+    def __getIndex_A(self, A: list):
         index = 0
         for i in range(len(A)):
-            index += 2**self.Phi.index(A[i]) # 找到集合A在框架列表中的对应索引
+            index += 2**self.Events.index(A[i]) # 找到集合A在框架列表中的对应索引
         return index
+
 
 
 # Main
 if __name__ == "__main__":
-    Phi = ["事件A", "事件B", "事件C"]
-    ds = DStheory(Phi)
-    print(ds.m)
-    ds.bpa(["事件A"], 0.2)
-    print(ds.m)
-
-    
+    Events = ["事件A", "事件B", "事件C"]
+    ds = DStheory(Events)
+    ds.addBPA(A=[["事件A", "事件B"], ["事件A", "事件C"]], m=[0.3, 0.7], name="BPA1")
+    ds.addBPA(A=[["事件A"], ["事件B"], ["事件C"]], m=[0.1, 0.2, 0.7], name="BPA2")
+    ds.showFrame()
     
 
